f570d6b5f3242867488e904ab1bdfb8cd0745a86
Ghos_h <soumyab.g@gmail.com>
2021-10-16 01:07:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.dataframe(token_url_df)
 
 token = st.text_input('TOKEN', '')
 if token_url_df['Token'].isin([token]).any().any():
-    st.dataframe(token_url_df.loc[token_url_df['Token']==token]['Invite Link'])
+    st.table(token_url_df.loc[token_url_df['Token']==token]['Invite Link'])
 else:
     st.write('Not a valid token')
 
