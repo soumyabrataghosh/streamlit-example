d300d87e3c748371c1e6c8dc4bf8a349fd935d98
Ghos_h <soumyab.g@gmail.com>
2021-10-16 01:19:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,12 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to Lustro22 Servey!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+### Enter your token
 """
 
-url_dict = {
-    'token1':'https://google.com',
-    'token2':'https://yahoo.com'
-}
 token_url_df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTjT51_uvt-sBRfDBdrWJCiCTspKxEtZTihm3RPb1YqtErzjSHbd1Sz0UZChsefSW1lD-z4Q66M4275/pub?output=csv')
-st.dataframe(token_url_df)
 
 token = st.text_input('TOKEN', '')
 if token_url_df['Token'].isin([token]).any().any():
