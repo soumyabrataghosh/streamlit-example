987cf177e7bd30a476702cfcde9e6b5d2956cf93
Ghos_h <soumyab.g@gmail.com>
2021-10-16 00:43:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ url_dict = {
     'token1':'https://google.com',
     'token2':'https://yahoo.com'
 }
+token_url_df = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vTjT51_uvt-sBRfDBdrWJCiCTspKxEtZTihm3RPb1YqtErzjSHbd1Sz0UZChsefSW1lD-z4Q66M4275/pub?output=xlsx')
+st.dataframe(token_url_df)
 
 token = st.text_input('TOKEN', '')
 if token in url_dict:
