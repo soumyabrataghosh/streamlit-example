bc8ea44ae7ec23d967476c24350d2f92bcd6b4b0
Ghos_h <soumyab.g@gmail.com>
2021-10-18 12:13:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,22 @@ import streamlit as st
 
 ### Enter your token
 """
-def print_hi():
-    st.error('Hello')
+def check_token(token_url_df_in,token_in):
+    if token_in == '':
+        pass
+    elif token_url_df_in['Token'].isin([token_in]).any().any():
+        url = list(token_url_df_in.loc[token_url_df['Token']==token_in]['Invite Link'])[0]
+        st.markdown("<a href='"+str(url)+"'> Go to your survey </a>",unsafe_allow_html=True)
+    else:
+        st.error('Not a valid token')
+
 
 token_url_df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTjT51_uvt-sBRfDBdrWJCiCTspKxEtZTihm3RPb1YqtErzjSHbd1Sz0UZChsefSW1lD-z4Q66M4275/pub?output=csv')
 
 token = st.text_input('TOKEN', '',max_chars=8)
 if st.button('Submit'):
-    print_hi()
+    check_token(token_url_df,token)
 
-if token == '':
-    pass
-elif token_url_df['Token'].isin([token]).any().any():
-    url = list(token_url_df.loc[token_url_df['Token']==token]['Invite Link'])[0]
-    st.markdown("<a href='"+str(url)+"'> Go to your survey </a>",unsafe_allow_html=True)
-else:
-    st.error('Not a valid token')
 
     
 footer="""<style>
